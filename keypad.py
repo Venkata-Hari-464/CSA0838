8f3f698c90b4b31ef81f86aa2243b4683bc523fd
Venkata Hari <112487363+Venkata-Hari-464@users.noreply.github.com>
2022-09-13 13:19:36 +0530

    Add files via upload

---
+++ keypad.py
@@ -0,0 +1,24 @@
+def letter_combinations(digits):
+    if digits == "":
+        return []
+    string_maps = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz",
+    }
+    result = [""]
+    for num in digits:
+        temp = []
+        for an in result:
+            for char in string_maps[num]:
+                temp.append(an + char)
+        result = temp
+    return result
+
+digit_string = input("enter the digits:")
+print(letter_combinations(digit_string))
