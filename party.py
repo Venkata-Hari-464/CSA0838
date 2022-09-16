3fbf956e6972164a709bfd68a5d74822293c33a1
Venkata Hari <112487363+Venkata-Hari-464@users.noreply.github.com>
2022-09-16 10:08:27 +0530

    Add files via upload

---
+++ party.py
@@ -0,0 +1,20 @@
+N=int(input("T="))
+if(N<0):
+    N=abs
+g=[]
+h=[]
+print("E=")
+for i in range(N):
+    x=int(input())
+    g.append(x)
+print("L=")
+for i in range(N):
+    y=int(input())
+    h.append
+sum=0
+max=sum
+for i in range(N):
+    sum=sum+g[i]-h[i]
+    if(max<sum):
+        max=sum
+print("Maximum number of guests on cruise at instance",max)
